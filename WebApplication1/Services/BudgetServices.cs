efa5ddc4e5fb009895acf067e552e56f254e2aec
Caden Lai <caden.lai@titansoft.com.sg>
2025-07-24 18:38:44 +0800

    refactor: rename and remove unused variable

--- WebApplication1/Services/BudgetServices.cs
+++ WebApplication1/Services/BudgetServices.cs
@@ -3,19 +3,18 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Services;
 
-public class BudgetServices(IBudgetRepo mockRepo)
+public class BudgetServices(IBudgetRepo budgetRepo)
 {
     public decimal Query(DateTime startDate, DateTime endDate)
     {
-        var budgets = mockRepo.GetAll();
-        var totalDays = (endDate - startDate).Days;
+        var budgets = budgetRepo.GetAll();
         decimal totalAmount = 0;
 
         for (var date = startDate; date <= endDate; date = date.AddDays(1))
         {
             var yearMonth = date.ToString("yyyyMM");
             var budget = budgets.FirstOrDefault(b => b.YearMonth == yearMonth);
-            
+
             if (budget != null)
             {
                 var daysInMonth = DateTime.DaysInMonth(date.Year, date.Month);
